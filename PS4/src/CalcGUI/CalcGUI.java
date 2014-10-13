679610ab86a35f3f9ce89b24a1fb0483e7fe49cf
Gary Sidoti <gsidoti@udel.edu>
2014-10-13 04:05:51 -0400

    finished

--- PS4/src/CalcGUI/CalcGUI.java
+++ PS4/src/CalcGUI/CalcGUI.java
@@ -65,50 +65,51 @@ public class CalcGUI {
 		frame.setBounds(100, 100, 450, 302);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		JButton btnCalculate = new JButton("Calculate");
 		btnCalculate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				double PValue = Double.parseDouble(txtPValue.getText());
 				double Years = Double.parseDouble(txtYears.getText());
-				double InterestRate = Double.parseDouble(txtInterestRate.getText());
+				double InterestRate = Double.parseDouble(txtInterestRate
+						.getText());
 				FValue proto = new FValue();
 				double Final = proto.returnFValue(PValue, InterestRate, Years);
-			    double FinalRnd = Math.round(Final*100.0)/100.0;
+				double FinalRnd = Math.round(Final * 100.0) / 100.0;
 				txtFValue.setText("$" + FinalRnd);
-				
+
 			}
 		});
 		btnCalculate.setBounds(166, 13, 234, 143);
 		frame.getContentPane().add(btnCalculate);
-		
+
 		txtInterestRate = new JTextField();
 		txtInterestRate.setBounds(22, 187, 116, 22);
 		frame.getContentPane().add(txtInterestRate);
 		txtInterestRate.setColumns(10);
-		
+
 		JLabel lblInvestmentAmount = new JLabel("Investment Amount");
 		lblInvestmentAmount.setBounds(12, 31, 116, 16);
 		frame.getContentPane().add(lblInvestmentAmount);
-		
+
 		txtYears = new JTextField();
 		txtYears.setBounds(22, 124, 116, 22);
 		frame.getContentPane().add(txtYears);
 		txtYears.setColumns(10);
-		
+
 		JLabel lblYears = new JLabel("Years");
 		lblYears.setBounds(12, 95, 56, 16);
 		frame.getContentPane().add(lblYears);
-		
+
 		txtPValue = new JTextField();
 		txtPValue.setBounds(22, 60, 116, 22);
 		frame.getContentPane().add(txtPValue);
 		txtPValue.setColumns(10);
-		
+
 		JLabel lblRateOfReturn = new JLabel("Rate of Return");
 		lblRateOfReturn.setBounds(12, 159, 116, 16);
 		frame.getContentPane().add(lblRateOfReturn);
-		
+
 		txtFValue = new JTextField();
 		txtFValue.setEditable(false);
 		txtFValue.setBounds(188, 183, 187, 30);
--- PS4/src/CalcUT/CalcUT.java
+++ PS4/src/CalcUT/CalcUT.java
@@ -4,14 +4,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import CalcEngine.FValue;
 
-
 public class CalcUT extends FValue {
-
 	@Test
 	public final void test() {
 		FValue test = new FValue();
-		double result = test.returnFValue(10000, .02, 20);
-		assertEquals(14859.473959783549,result,.001);
+		double result = test.returnFValue(10000, 3, 3.5);
+		assertEquals(11022.77, result, .001);
 	}
-
 }
